c422db774920a85129d029fe460bbcb034a541b0
윤종서 <yunjongseo@yunjongseoui-MacBookAir.local>
2019-10-06 16:19:02 +0900

    -

---
+++ yamigu/Wating/FilterView/FilterView.swift
@@ -0,0 +1,21 @@
+//
+//  FilterView.swift
+//  yamigu
+//
+//  Created by 윤종서 on 2019/10/05.
+//  Copyright © 2019 Yoon. All rights reserved.
+//
+
+import UIKit
+
+class FilterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
