f7a7be5df72e25c8763419a59883b4a0f087a1e2
jong <yjs@brint.info>
2019-10-06 18:51:43 +0900

    more ui

---
+++ yamigu/More/MoreCell/QuestionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionTableViewCell.swift
+//  yamigu
+//
+//  Created by Yoon on 06/10/2019.
+//  Copyright © 2019 Yoon. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
