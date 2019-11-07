b094409f9bd7f782ffb848dafaefedd10a96910a
ph7164 <ph7164@hongpilhwaui-MacBook-Air.local>
2019-11-07 15:06:44 +0900

    add logout postrequest

--- yamigu/More/SettingVC.swift
+++ yamigu/More/SettingVC.swift
@@ -27,10 +27,23 @@ class SettingVC: UIViewController {
         setupUI()
     }
     
-    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+    @objc func logoutTap(_ sender: UITapGestureRecognizer? = nil) {
         let logoutAlert = UIAlertController(title: "로그아웃", message: "로그아웃 하시겠습니까?", preferredStyle: UIAlertController.Style.alert)
 
         logoutAlert.addAction(UIAlertAction(title: "로그아웃", style: .default, handler: { (action: UIAlertAction!) in
+            print("Handle Ok logic here")
+            self.postRequest("http://147.47.208.44:9999/api/auth/logout/")
+        }))
+
+        logoutAlert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+
+        present(logoutAlert, animated: true, completion: nil)
+    }
+    
+    @IBAction func withdrawalBtnPressed(_ sender: Any) {
+        let logoutAlert = UIAlertController(title: "회원탈퇴", message: "회원탈퇴시 3개월간 재가입이 불가능합니다.\n정말 탈퇴하시겠습니까?", preferredStyle: UIAlertController.Style.alert)
+
+        logoutAlert.addAction(UIAlertAction(title: "회원탈퇴", style: .default, handler: { (action: UIAlertAction!) in
             print("Handle Ok logic here")
         }))
 
@@ -38,12 +51,52 @@ class SettingVC: UIViewController {
 
         present(logoutAlert, animated: true, completion: nil)
     }
+    
 
+    func postRequest(_ urlString: String){
+        guard let url = URL(string: urlString) else {return}
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.setValue("Token \(authKey)", forHTTPHeaderField: "Authorization")
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let res = response{
+                
+                print(res)
+                
+            }
+            if let data = data {
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                    
+                    guard let newValue = json as? Dictionary<String, String> else {
+                        print("invalid format")
+                        return
+                        
+                    }
+                    
+                    DispatchQueue.main.async {
+                        
+                        //self.dismiss(animated: false, completion: nil)
+                    }
+                } catch {
+                    print(error)
+                    
+                    
+                }
+            }
+        }.resume()
+    }
 }
 
 extension SettingVC {
     func setupUI() {
-        let logoutTap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+        let logoutTap = UITapGestureRecognizer(target: self, action: #selector(self.logoutTap(_:)))
         self.view_logout.addGestureRecognizer(logoutTap)
     }
+    
+    
 }
